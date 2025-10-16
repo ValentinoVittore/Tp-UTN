8cf1a77676736202601189197a4ac1838f3ff90f
ValentinoVittore <valevittore@hotmail.com.ar>
2025-10-16 18:33:02 -0300

    Introducción a la programación estructurada‬.c

--- Introducción a la programación estructurada‬.c
+++ Introducción a la programación estructurada‬.c
@@ -10,22 +10,20 @@ int calificacion;
     if(calificacion >= 90){
         printf("Calificacion: A");
     }
-
-    if(calificacion >=80 & calificacion < 90 ){
+    else if(calificacion >=80 && calificacion < 90 ){
         printf("Calificacion: B");
     }
 
-    if(calificacion >= 70 & calificacion < 80){
+    else if(calificacion >= 70 && calificacion < 80){
         printf("Calificacion: C");
     }
 
-    if(calificacion >= 60 & calificacion < 70){
+    else if(calificacion >= 60 && calificacion < 70){
         printf("Calificacion: D");
     }
 
-    if(calificacion < 60){
+   else{
         printf("Calificacion: F");
     }
-
 return 0;
 }
